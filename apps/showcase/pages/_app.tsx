b3501151c1da0b0f32ee01fb49b3ae53db80b880
dmaisano <dmaisanooo@gmail.com>
2024-07-09 00:12:25 -0400

    chore: Add favicon to showcase app

--- apps/showcase/pages/_app.tsx
+++ apps/showcase/pages/_app.tsx
@@ -1,7 +1,15 @@
 import "@repo/ui/global.css";
+import Head from "next/head";
 
 import type { AppProps } from "next/app";
 
 export default function NextApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
